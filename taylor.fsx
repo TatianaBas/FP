2fe69a981ed4fe9e7dc426b1114649a568c5f3da
TatianaBas <145323065+TatianaBas@users.noreply.github.com>
2024-03-17 14:26:54 +0300

    Update taylor.fsx

--- taylor.fsx
+++ taylor.fsx
@@ -1,25 +1,42 @@
 // Print a table of a given function f, computed by taylor series
-
+//3 вариант
 // function to compute
-let f = sin
+open System
 
-let a = 0.0
-let b = 1.0
+let builtinCalc x = Math.Log(1.0 + x - 2.0*x*x)
+
+let rec taylor_naive_log (x: float) (n: float) (i: float) (acc: float) (epsilon: float) =
+    let prev = ((-1.)**(i+1.) * 2.**i - 1.) * (x**i) / i
+    let cur = ((-1.)**i * 2.**i - 1.) * (x**(i+1.)) / (i+1.)
+    if (abs(cur - prev) < epsilon) then
+        (acc, i)
+    else
+        let acc = acc + prev
+        taylor_naive_log x n (i + 1.) acc epsilon
+
+
+
+let smartTaylor x =
+    let rec loop n acc result =
+        let newAcc = ((-1.0) ** (float (n+1)) * 2.0**float(n) - 1.0) * (x ** float n) / float n
+        let newResult = acc + newAcc
+        if Math.Abs(newAcc) < 0.0001 then newResult, n
+        else loop (n+1) newResult newResult
+    loop 1 0.0 0.0
 let n = 10
-
-// Define a function to compute f using naive taylor series method
-let taylor_naive = f
-
-
-// Define a function to do the same in a more efficient way
-let taylor = f
+let a = -0.2
+let b = 0.3
+let epsilon = 0.0001
+printfn "| x     | Builtin | Smart Taylor | # terms | Dumb Taylor | # terms |"
 
 let main =
-   for i=0 to n do
-     let x = a+(float i)/(float n)*(b-a)
-     printfn "%5.2f  %10.6f  %10.6f   %10.6f" x (f x) (taylor_naive x) (taylor x)
-// make sure to improve this table to include the required number of iterations
-// for each of the methods
+    for i = 0 to n do
+        let x = a + (float i) / (float n) * (b - a)
+        let xFloat = float x
+        let builtinResult = builtinCalc xFloat
+        let smartResult, smartTerms = smartTaylor xFloat
+        let (taylor_naive_res, taylor_naive_iter) = taylor_naive_log x i 1. 0. epsilon
+        printfn "| %.2f | %.4f | %.4f | %d | %.4f | %0.0f |" xFloat builtinResult smartResult smartTerms taylor_naive_res taylor_naive_iter
 
 main
 
